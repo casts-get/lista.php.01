58e4838b3ef2c6959ed0ed2169ed8656aaeff180
casts-get <Carine_dos-santos@estudante.sesisenai.org.br>
2026-07-08 08:59:07 -0300

    add-exercicio 4

---
+++ ex_04.php
@@ -0,0 +1,20 @@
+<?php
+
+function gerarSenha($quantidade){
+
+    $caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*?";
+
+    $senha = "";
+
+    for($i = 0; $i < $quantidade; $i++){
+
+        $posicao = rand(0, strlen($caracteres)-1);
+
+        $senha .= substr($caracteres, $posicao, 1);
+
+    }
+
+    return $senha;
+}
+
+echo gerarSenha(8);
